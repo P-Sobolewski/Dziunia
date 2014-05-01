9e183aa682c7d5a24e475d8c05ef7848c973135a
Paweł Sobolewski <pawelsobolewski@MacBook-Pro-Pawe.local>
2014-05-01 12:53:28 +0200

    przeniesienie FP do pliku

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,22 +1,4 @@
-start
-<br/>
 <?php
-
-$wartosc1 = 3;
-$wartosc2 = 5;
-
-/*
-// tutaj wyswietlamy Hello World
-$tekst1 = 'Hello';
-$tekst2 = 'World';
-echo $tekst1 . ' ' . $tekst2;
-*/
-
-//var_dump($wartosc1);
-
-//fizzBuzz($wartosc1);
-fizzBuzz();
-
 function fizzBuzz($maxValue = 15) {
    
 	for ($i = 1; $i <= $maxValue; $i++) {
