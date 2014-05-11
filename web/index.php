44ead5ee024e2c472ca3a166a396ab6e90697ea5
P-Sobolewski <sobolewski85@gmail.com>
2014-05-11 16:47:52 +0200

    Pierwsza silexowa apka

--- web/index.php
+++ web/index.php
@@ -1,10 +1,27 @@
+<?php
+/*
 start
 <br/>
 
-<?php
 include_once("../FizzBuzz.php");
 include_once("../obrazek.php");
 
 
 //fizzBuzz();
 obrazek();
+*/
+require_once __DIR__.'/../vendor/autoload.php'; 
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/hello/{name}', function($name) use($app) { 
+    return 'Hello '.$app->escape($name); 
+});
+
+$app->get('/', function() use ($app) {
+    return 'Welcome in Dziunia!'; 
+});
+
+$app->run();
