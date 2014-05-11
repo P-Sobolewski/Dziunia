e46fff617a7a44bc0f98809025041a46fa45bfc9
Paweł Sobolewski <pawelsobolewski@PabloMac.local>
2014-05-11 15:51:11 +0200

    przeniesienie index php do web

---
+++ obrazek.php
@@ -0,0 +1,12 @@
+<?php
+
+function obrazek(
+        $imageSource = 'http://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/250px-PHP-logo.svg.png'
+)
+{
+
+
+echo "<img src='$imageSource'>";
+
+
+}
---
+++ web/index.php
@@ -0,0 +1,10 @@
+start
+<br/>
+
+<?php
+include_once("../FizzBuzz.php");
+include_once("../obrazek.php");
+
+
+//fizzBuzz();
+obrazek();
